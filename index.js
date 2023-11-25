63f21de533693307bcb6b8b814e547637aeeed15
Harmony Burke <theharmonyburke@gmail.com>
2023-11-25 09:37:41 -0600

    added logo class into logo.js, started functions to write the file, looking for bug in setText

--- index.js
+++ index.js
@@ -1,38 +1,73 @@
-const inquirer=require('inquirer')
-const fs=require('fs')
+const inquirer = require('inquirer')
+// const fs = require('fs')
+const Logo = require('./lib/logo.js')
+const { Circle, Triangle, Square } = require('./lib/shapes.js')
+const { writeFile } = require('fs/promises')
 
 
-const prompts =[
-    {
-        type:'input',
-        message: 'Enter your 3 character logo name.',
-        name:'logoName' ,  
-    },
-    {
-        type:'input',
-        message:'What color would you like the text to be?',
-        name: 'color',
-    },
-    {
-        type:'list',
-        message:'What shape would you like to use?',
-        choices: ['circle', 'triangle', 'sqaure'],
-        name:'shape',
-    },
-    {
-        type: 'input',
-        message:'What color would you like the shape to be?',
-        name:'shapeColor'
-    }
-
-];
-
-
+function prompts() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: 'Enter your 3 character logo name.',
+            name: 'logoName',
+        },
+        {
+            type: 'input',
+            message: 'What color would you like the text to be?',
+            name: 'color',
+        },
+        {
+            type: 'list',
+            message: 'What shape would you like to use?',
+            choices: ['circle', 'triangle', 'square'],
+            name: 'shape',
+        },
+        {
+            type: 'input',
+            message: 'What color would you like the shape to be?',
+            name: 'shapeColor'
+        }
+    ])
 
 
-function init(){
-    inquirer.prompt(prompts)
+
+        .then(({ logoName, color, shape, shapeColor }) => {
+            let chosenShape
+            switch (shape) {
+                case "circle":
+                    chosenShape = new Circle()
+                    break;
+                case 'triangle':
+                    chosenShape = new Triangle()
+                    break;
+                case 'square':
+                    chosenShape = new Square()
+                    break;
+
+            }
+            chosenShape.setColor(shapeColor)
+            // begin function for logo
+            const logo = new Logo()
+            logo.setText(logoName, color)
+            logo.setShape(chosenShape)
+            return writeFile('logo.svg', logo.render())
+        })
+        .then(() => {
+            console.log('Your logo has been created!')
+        })
+        .catch((error) => {
+            console.log('An error has occurred.', error)
+        })
+
 }
+prompts()
+// function init(){
+//     inquirer.prompt(prompts)
+//     // .then(data){
+//     //     fs.writeFile('logo.svg', )
+//     // }
+// }
 
 
-init()
+// init()
